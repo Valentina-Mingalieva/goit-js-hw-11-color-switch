17afa1c6bde8435139be1c06d98bbbb27139c60c
Valentina Mingalieva <valentina.mingalieva@gmail.com>
2021-06-20 14:52:14 +0200

    fast

---
+++ index.js
@@ -0,0 +1,33 @@
+const colors = [
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+];
+
+const start = document.querySelector('[data-action="start"]');
+const stop = document.querySelector('[data-action="stop"]');
+const body = document.querySelector('body');
+let changeColor = null;
+
+// Для генерации случайного числа (индекс элемента массива цветов)
+const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}; 
+
+start.addEventListener('click', () => {        
+    changeColor = setInterval(() => {            
+        body.style.backgroundColor = colors[randomIntegerFromInterval(colors[0], colors.length)];
+    }, 1000);
+
+    start.setAttribute('disabled', true);
+    stop.removeAttribute('disabled');
+});
+
+stop.addEventListener('click', () => {
+    clearInterval(changeColor);
+    stop.setAttribute('disabled', true);
+    start.removeAttribute('disabled');
+});
